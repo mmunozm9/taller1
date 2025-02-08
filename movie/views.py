9cb3a43ec29428d49a5cfb8cfc8adf1b76aed2d5
Mariana Muñoz <marianamunoz@MacBook-Pro-de-Mariana.local>
2025-02-07 21:19:24 -0500

    se agrega el azul otra vez

--- movie/views.py
+++ movie/views.py
@@ -4,13 +4,18 @@ from .models import Movie
 
 # Create your views here.
 def home(request):
-    #return render(request, 'home.html', {'name': 'Mariana Muñoz'})
     searchTerm = request.GET.get('searchMovie')
+
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
+
+    return render(request, 'home.html', {
+        'name': 'Mariana Muñoz',
+        'searchTerm': searchTerm,
+        'movies': movies
+    })
 
 def about(request):
     return render(request, 'about.html')
